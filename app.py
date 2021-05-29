56868ee0645ec4e63b9d53ea8b7771015077a563
Akshay <62572713+root-Akshay@users.noreply.github.com>
2021-05-29 23:28:12 +0530

    Updated app.py
    Fixed no audio for downloaded videos

--- app.py
+++ app.py
@@ -7,17 +7,37 @@ from werkzeug.utils import redirect
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+def get_resolutions(streams):
+  streams = streams.filter(progressive=True)
+  x = [str(s).split() for s in streams]
+  resolutions = [r[3].strip('res="') for r in x] #creates a list of availiable resolution with audio and video
+  return resolutions
+
+def get_stream_for_res(streams, res):
+  #Filter on the basis of a given resolution and return a list of filtered streams
+  streams = streams.filter(progressive=True)
+  stream = list(filter(lambda x: x.resolution == res, streams))
+  return stream
+
+
 @app.route('/', methods=['GET','POST'])
 def homepage():
 
   if request.method == 'POST':
+    
     try:
       youtube_obj = YouTube(request.form['link'].strip())
       res =  request.form['res'].strip()
 
-      req_stream_obj= get_stream_for_res(youtube_obj.streams,res)[0] 
-      req_stream_obj.download()
-      flash(f"YouTube Video {youtube_obj.title} Download with Resolution {res}")
+      resolutions = get_resolutions(youtube_obj.streams)
+
+      if res in resolutions:
+        req_stream_obj= get_stream_for_res(youtube_obj.streams,res)[0] 
+        req_stream_obj.download()
+        flash(f"YouTube Video {youtube_obj.title} Download with Resolution {res}")
+      else:
+        flash('Selected resolution has no Audio')
+        return render_template('homepage.html',resolutions=resolutions)
 
     except:
       flash('Unable to Download try changing the resoltion')
@@ -26,7 +46,3 @@ def homepage():
   return render_template('homepage.html')
 
 
-def get_stream_for_res(streams, res):
-  #Filter on the basis of a given resolution and return a list of filtered streams
-  stream = list(filter(lambda x: x.resolution == res, streams))
-  return stream
